8d381fbfd1627492a180a41a7cb3ee70909c4159
Guilherme Fernandes <guilhermefs09@protonmail.com>
2024-02-20 21:00:30 -0300

    Variaveis de ambiente atualizadas

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -97,10 +97,10 @@ REST_FRAMEWORK = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': "localhost", #os.environ.get('DB_HOST'),
-        'NAME': "mydb", #os.environ.get('DB_NAME'),
-        'USER': "devuser", #os.environ.get('DB_USER'),
-        'PASSWORD': "changeme" #os.environ.get('DB_PASS'),
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 
