b581eede6a4cad439a8c118697a2348f60c5c335
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-03-03 16:01:56 -0300

    deploy: uwsgi config

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -153,8 +153,8 @@ USE_TZ = True
 STATIC_URL = '/static/static/'
 MEDIA_URL = '/static/media/'
 
-MEDIA_ROOT = '/vol/web/media'
-STATIC_ROOT = '/vol/web/static'
+MEDIA_ROOT = '/vol/web/media/'
+STATIC_ROOT = '/vol/web/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
