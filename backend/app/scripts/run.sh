b95f70e33fd18e1fd3aaef58e45284e4578726d6
Andre da Silva Carmona <andre.carmona@ymail.com>
2024-03-05 21:43:21 -0300

    cmt

--- backend/app/scripts/run.sh
+++ backend/app/scripts/run.sh
@@ -4,9 +4,8 @@ set -e
 
 python /app/manage.py wait_for_db
 python /app/manage.py collectstatic --noinput
-python /app/manage.py migrate --run-syncdb
 python /app/manage.py makemigrations users
 python /app/manage.py makemigrations api
-python /app/manage.py migrate
+python /app/manage.py migrate --run-syncdb
 
-uwsgi --socket :9000 --workers 2 --master --enable-threads --module app.config.wsgi --http-timeout 86400
+uwsgi --socket :9000 --workers 2 --master --enable-threads --module app.config.wsgi
