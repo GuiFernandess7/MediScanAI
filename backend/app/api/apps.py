a7ebd1919b1de3ebae06d2bac04ebdca22c26cf4
Guilherme Fernandes <guilhermefs09@protonmail.com>
2024-03-05 23:20:45 -0300

    Caminho adicionado a apps.py

--- backend/app/api/apps.py
+++ backend/app/api/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
+from django.conf import settings
+import os
 
 
 class ApiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "api"
+    path = os.path.join(settings.BASE_DIR, 'api')
